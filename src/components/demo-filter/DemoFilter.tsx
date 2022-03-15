25cff9c1c924b0e2b0053f4f489512a87273ed35
Borna <borna.bezjak@ingemark.com>
2022-03-15 17:17:11 +0100

    in DemoFilter add form and to fields abd validation for them

--- src/components/demo-filter/DemoFilter.tsx
+++ src/components/demo-filter/DemoFilter.tsx
@@ -1,7 +1,48 @@
-import { Button, Flex, FormControl, FormLabel, Heading, HStack, Input, Spacer, VStack } from "@chakra-ui/react"
+import { Button, Flex, FormControl, FormLabel, Heading, HStack, Input, NumberInput, NumberInputField, Select, Spacer, VStack } from "@chakra-ui/react"
+import { useState } from "react";
 
 export const DemoFilter = (props: any) => {
 
+    const [select, setSelected] = useState<string>();
+    const [from, setFrom] = useState<string | undefined>();
+    const [fromInvalid, setFromInvalid] = useState<boolean>();
+    const [to, setTo] = useState<string | undefined>();
+    const [toInvalid, setToInvalid] = useState<boolean>();
+
+    const handleSelect = (value: string) => {
+        console.log(value)
+
+        setSelected(value);
+    }
+
+    const handleFrom = (value: string) => {
+        setFromInvalid(false);
+        setFrom(value);
+    }
+
+    const handleFromBlur = (value: string) => {
+        let newValue: string = value;
+        if(to && Number.parseInt(value) > Number.parseInt(to)) {
+            newValue = to;
+            setFromInvalid(true);
+        }
+        setFrom(newValue);
+    }
+
+    const handleTo = (value: string) => {
+        setToInvalid(false);
+        setTo(value);
+    }
+
+    const handleToBlur = (value: string) => {
+        let newValue = value;
+        if(from && Number.parseInt(value) < Number.parseInt(from)) {
+            newValue = from;
+            setToInvalid(true);
+        }
+        setTo(newValue);
+    }
+    
     return (
         <VStack w="full" h="full" align="flex-start" bg="gray.50" px={10} pb={10} pt={5}>
 
@@ -13,18 +54,26 @@ export const DemoFilter = (props: any) => {
                 <HStack spacing='24px'>
 
                     <FormControl>
-                        <FormLabel>First Name</FormLabel>
-                        <Input placeholder="John" />
+                        <FormLabel>Some random option</FormLabel>
+                        <Select placeholder='Select option' onChange={(e) => handleSelect(e.target.value)} value={select}>
+                            <option value='option1'>Option 1</option>
+                            <option value='option2'>Option 2</option>
+                            <option value='option3'>Option 3</option>
+                        </Select>
                     </FormControl>
 
-                    <FormControl>
-                        <FormLabel>First Name</FormLabel>
-                        <Input placeholder="John" />
+                    <FormControl isInvalid={fromInvalid}>
+                        <FormLabel>From</FormLabel>
+                        <NumberInput placeholder="0" value={from} onChange={(valueString) => handleFrom(valueString)} onBlur={(e) => handleFromBlur(e.target.value)} >
+                            <NumberInputField />
+                        </NumberInput>
                     </FormControl>
 
-                    <FormControl>
-                        <FormLabel>First Name</FormLabel>
-                        <Input placeholder="John" />
+                    <FormControl isInvalid={toInvalid}>
+                        <FormLabel>To</FormLabel>
+                        <NumberInput placeholder="10" value={to} onChange={(valueString) => handleTo(valueString)} onBlur={(e) => handleToBlur(e.target.value)} >
+                            <NumberInputField />
+                        </NumberInput>
                     </FormControl>
 
                 </HStack>
